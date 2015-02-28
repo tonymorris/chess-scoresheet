768d3bcf4acc4361443801346f82d18e797967f1
Tony Morris <tmorris@tmorris.net>
2015-02-28 23:20:36 +1000

    formatting

--- src/Data/Chess/Scoresheet.hs
+++ src/Data/Chess/Scoresheet.hs
@@ -12,14 +12,30 @@ circ =
 
 ----
 
-node :: Int -> Diagram B R2
-node n = text (show n) # fontSizeN 0.1 # fc white <> circle 0.2 # fc green # named n
+node ::
+  Int
+  -> Diagram B R2
+node n =
+  text (show n) #
+  fontSizeN 0.1 #
+  fc white <> circle 0.2 #
+  fc green #
+  named n
 
-arrowOpts :: ArrowOpts
-arrowOpts = with & gaps  .~ small & headLength .~ Global 0.2
+arrowOpts ::
+  ArrowOpts
+arrowOpts =
+  with & gaps  .~ small & headLength .~ Global 0.2
 
-tournament :: Int -> Diagram B R2
-tournament n = decorateTrail (regPoly n 1) (map node [1..n]) # applyAll [connectOutside' arrowOpts j k | j <- [1 .. n-1], k <- [j+1 .. n]]
+tournament ::
+  Int
+  -> Diagram B R2
+tournament n =
+  decorateTrail (regPoly n 1) (map node [1..n]) #
+  applyAll [connectOutside' arrowOpts j k | j <- [1 .. n-1], k <- [j+1 .. n]]
  
-example :: Diagram B R2
-example = tournament 6 # connectOutside' (with & gaps .~ small & headLength .~ Global 0.2) (1 :: Int) (2 :: Int)
+example ::
+  Diagram B R2
+example =
+  tournament 6 #
+  connectOutside' (with & gaps .~ small & headLength .~ Global 0.2) (1 :: Int) (2 :: Int)
