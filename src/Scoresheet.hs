2f8fd27f2276a8a87297e610a6ce15b08e83eb39
Tony Morris <tmorris@tmorris.net>
2015-03-09 13:28:16 +1000

    test

--- src/Scoresheet.hs
+++ src/Scoresheet.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Data.Chess.Scoresheet
-import Data.Chess.OutputFormat
 import Data.Maybe
 import Diagrams.Prelude
 import System.IO(IO)
@@ -9,4 +8,4 @@ import System.IO(IO)
 main ::
   IO ()
 main =
-  renderChessScoresheet PDF' (mkSizeSpec (Just 800) Nothing)
+  renderChessScoresheets (mkSizeSpec (Just 800) Nothing)
