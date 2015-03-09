1860513966ac27961f2fb0e47bb4a225f7992d99
Tony Morris <tmorris@tmorris.net>
2015-03-09 11:26:55 +1000

    remove comments

--- src/Data/Chess/Scoresheet.hs
+++ src/Data/Chess/Scoresheet.hs
@@ -10,47 +10,6 @@ import Prelude
 import System.Directory
 import System.FilePath
 
-{-
-
--- examples for learning diagrams
-
-circ ::
-  Diagram B R2
-circ =
-  circle 1 # fc red # lw none ||| circle 1 # fc green # lw none
-
-----
-
-node ::
-  Int
-  -> Diagram B R2
-node n =
-  text (show n) #
-  fontSizeN 0.1 #
-  fc white <> circle 0.2 #
-  fc green #
-  named n
-
-arrowOpts ::
-  ArrowOpts
-arrowOpts =
-  with & gaps  .~ small & headLength .~ Global 0.2
-
-tournament ::
-  Int
-  -> Diagram B R2
-tournament n =
-  decorateTrail (regPoly n 1) (map node [1..n]) #
-  applyAll [connectOutside' arrowOpts j k | j <- [1 .. n-1], k <- [j+1 .. n]]
- 
-example ::
-  Diagram B R2
-example =
-  tournament 6 #
-  connectOutside' (with & gaps .~ small & headLength .~ Global 0.2) (1 :: Int) (2 :: Int)
-
--}
-
 blackshading ::
   Colour Double
 blackshading =
