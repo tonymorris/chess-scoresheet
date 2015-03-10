ec32d18b70b1c3b5aa276e31dca09979c8d63b93
Tony Morris <tmorris@tmorris.net>
2015-03-10 12:44:40 +1000

    align logo to the left, per advice from Chris Mears on the diagrams mailing list

--- src/Data/Chess/Scoresheet.hs
+++ src/Data/Chess/Scoresheet.hs
@@ -25,9 +25,9 @@ logowithurl ::
 logowithurl =
   do l <- logo
      let t = text "http://thegapchessclub.org.au" # font "DejaVu Sans Mono" # fontSizeN 0.01 # fc darkblue
-         r = rect 200 10 # lc white
+         r = rect 140 10 # lc white
      return ((t <> r) === l)
-
+     
 namebox ::
   Diagram B R2
 namebox =
