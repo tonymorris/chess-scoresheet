59084bd1dedf50e123be92dc787068e59f6ada2a
Tony Morris <tmorris@tmorris.net>
2015-07-24 10:05:57 +1000

    strip

--- src/Data/Chess/Scoresheet.hs
+++ src/Data/Chess/Scoresheet.hs
@@ -12,39 +12,44 @@ import Prelude hiding (round)
 import System.Directory
 import System.FilePath
 
-logowithurl ::
+logowithtext ::
   IO (Diagram B)
-logowithurl =
+logowithtext =
   let logowidth = 420
       logoheight = 232
       logoscale = 3.5
       logodims = dims2D (logowidth / logoscale) (logoheight / logoscale)
   in
     do l <- fmap (either text (\l -> image l # sized logodims)) (loadImageExt "etc/tgcc-logo.png")
-       let t = text "http://thegapchessclub.org.au"
+       let t = text "logo text"
            r = rect 140 10 # lw none
        return ((t <> r) === l)
     
-namebox ::
+box1 ::
   Diagram B
-namebox =
+box1 =
   let textbox c = alignedText (-0.1) 0 c 
-      labelbox c w = textbox c <> rect w 40 # lwL 1.2 # alignL
-      box c = hcat' (with & sep .~ 20) [labelbox c 290, labelbox "baby bear" 78] # centerX
-  in box "papa bear" 
+      labelbox c w = textbox c <> rect w 40 # alignL
+  in hcat' (with & sep .~ 20) [labelbox "papa bear" 290, labelbox "baby bear" 78] # centerX
+
+box2 ::
+  Diagram B
+box2 =
+  let textbox c = alignedText (-0.1) (-0.3) c
+      labelbox c w = textbox c <> rect w 40 # lwL 1.2 # alignL      
+  in labelbox "mama bear" 242
 
 scoresheet ::
   Diagram B
   -> Diagram B
 scoresheet l =
-   vcat' (with & sep .~ 5) [l # alignL, namebox # alignL]
+   vcat' (with & sep .~ 5) [(hcat' (with & sep .~ 10) [l, box2]) # alignL, box1 # alignL]
   
-detailbox ::
-  Diagram B
-detailbox =
-  let textbox c = alignedText (-0.1) (-0.3) c
-      labelbox c w = textbox c <> rect w 40 # fc white # lwL 1.2 # alignL      
-  in labelbox "mama bear" 242
+scoresheetwithlogo ::
+  IO (Diagram B)
+scoresheetwithlogo =
+  do l <- logowithtext
+     return (scoresheet l)
 
 renderChessScoresheet ::
   OutputFormat 
@@ -54,8 +59,8 @@ renderChessScoresheet t s =
   let outputDirectory = "out"
       options = CairoOptions (outputDirectory </> "chess-scoresheet" <//> t) s (formatType t) False
   in do createDirectoryIfMissing True outputDirectory
-        l <- logowithurl
-        fst (renderDia Cairo options (scoresheet (hcat' (with & sep .~ 10) [l, detailbox])))
+        ss <- scoresheetwithlogo
+        fst (renderDia Cairo options ss)
 
 renderChessScoresheets ::
   SizeSpec V2 Double
