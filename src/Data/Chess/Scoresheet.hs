e91e60b8a1fa57558cbc29c526147ac0a3a86d8e
Tony Morris <tmorris@tmorris.net>
2015-07-31 15:51:10 +1000

    strip

--- src/Data/Chess/Scoresheet.hs
+++ src/Data/Chess/Scoresheet.hs
@@ -12,44 +12,11 @@ import Prelude hiding (round)
 import System.Directory
 import System.FilePath
 
-logowithtext ::
-  IO (Diagram B)
-logowithtext =
-  let logowidth = 420
-      logoheight = 232
-      logoscale = 3.5
-      logodims = dims2D (logowidth / logoscale) (logoheight / logoscale)
-  in
-    do l <- fmap (either text (\l -> image l # sized logodims)) (loadImageExt "etc/tgcc-logo.png")
-       let t = text "logo text"
-           r = rect 140 10 # lw none
-       return ((t <> r) === l)
-    
-box1 ::
+box ::
   Diagram B
-box1 =
-  let textbox c = alignedText (-0.1) 0 c 
-      labelbox c w = textbox c <> rect w 40 # alignL
-  in hcat' (with & sep .~ 20) [labelbox "papa bear" 290, labelbox "baby bear" 78] # centerX
-
-box2 ::
-  Diagram B
-box2 =
-  let textbox c = alignedText (-0.1) (-0.3) c
-      labelbox c w = textbox c <> rect w 40 # lwL 1.2 # alignL      
-  in labelbox "mama bear" 242
-
-scoresheet ::
-  Diagram B
-  -> Diagram B
-scoresheet l =
-   vcat' (with & sep .~ 5) [(hcat' (with & sep .~ 10) [l, box2]) # alignL, box1 # alignL]
-  
-scoresheetwithlogo ::
-  IO (Diagram B)
-scoresheetwithlogo =
-  do l <- logowithtext
-     return (scoresheet l)
+box =
+  let labelbox c w = text c <> rect w 40 # lwL 1.2 # alignL      
+  in labelbox "abcdefghijkl" 242
 
 renderChessScoresheet ::
   OutputFormat 
@@ -59,8 +26,7 @@ renderChessScoresheet t s =
   let outputDirectory = "out"
       options = CairoOptions (outputDirectory </> "chess-scoresheet" <//> t) s (formatType t) False
   in do createDirectoryIfMissing True outputDirectory
-        ss <- scoresheetwithlogo
-        fst (renderDia Cairo options ss)
+        fst (renderDia Cairo options box)
 
 renderChessScoresheets ::
   SizeSpec V2 Double
--- src/Scoresheet.hs
+++ src/Scoresheet.hs
@@ -1,12 +1,50 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Main where
 
-import Data.Chess.Scoresheet
--- import Data.Test
 import Data.Maybe
 import Diagrams.Prelude
-import System.IO(IO)
+import Diagrams.Backend.Cairo
+import Diagrams.Backend.Cairo.Internal
+import Prelude hiding (round)
+
+box ::
+  Diagram B
+box =
+  let labelbox c w = text c <> rect w 40 # lwL 1.2 # alignL      
+  in labelbox "abcdefghijkl" 242
+
+fileout ::
+  OutputType
+  -> String
+  -> String
+fileout PNG =
+  (++ ".png")
+fileout PDF =
+  (++ ".pdf")
+fileout PS =
+  (++ ".ps")
+fileout SVG =
+  (++ ".svg")
+fileout RenderOnly =
+  id
+
+renderout ::
+  OutputType 
+  -> SizeSpec V2 Double
+  -> IO ()
+renderout t s =
+  let options = CairoOptions (fileout t "out") s t False
+  in fst (renderDia Cairo options box)
+
+renderouts ::
+  SizeSpec V2 Double
+  -> IO ()
+renderouts s =
+  mapM_ (`renderout` s) [PNG ..]
 
 main ::
   IO ()
 main =
-  renderChessScoresheets (mkSizeSpec (V2 (Just 800) Nothing))
+  renderouts (mkSizeSpec (V2 (Just 800) Nothing))
